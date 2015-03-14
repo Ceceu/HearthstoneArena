0761176aa4a7178ba266fc6fa4434d80a5475011
Celso <Celso@MarkOne>
2015-03-14 19:43:14 -0300

    initial commit.

--- src/hearthstonearena/HearthstoneArena.java
+++ src/hearthstonearena/HearthstoneArena.java
@@ -15,7 +15,7 @@ public class HearthstoneArena {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("This is HearthStone Simulator");
     }
     
 }
